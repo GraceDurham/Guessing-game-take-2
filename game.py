b982ca2493535e8aceb197f1056eff755aaf4f8e
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2017-03-29 15:36:52 -0700

    Confirm guess falls within range

--- game.py
+++ game.py
@@ -10,12 +10,16 @@ while True:
     your_guess = int(raw_input("Your guess?"))
     number_guesses = number_guesses + 1
     
-    if your_guess < my_number:
-        print "Your guess is too low. try again."
-    
-    elif your_guess > my_number:
-        print "Your guess is too high. try again."
-    
-    else:
-        print "Well done, %s! You found my number in %s tries." % (name,number_guesses)
-        break
+    if your_guess < 1 or your_guess > 100:
+        print " Hey now that is not a number between 1 or 100. Please follow the instructions and pick a number between 1 and 100."
+
+    else:   
+        if your_guess < my_number:
+            print "Your guess is too low. try again."
+        
+        elif your_guess > my_number:
+            print "Your guess is too high. try again."
+        
+        else:
+            print "Well done, %s! You found my number in %s tries." % (name,number_guesses)
+            break
